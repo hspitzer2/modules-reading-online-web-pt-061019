fa81fad3a01077df41955cdbcf10c72dba57626c
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-23 16:04:15 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,27 +1,27 @@
 module FancyDance
   module InstanceMethods
 
-  def twirl
-    "I'm twirling!"
+    def twirl
+      "I'm twirling!"
+    end
+
+    def jump
+      "Look how high I'm jumping!"
+    end
+
+    def pirouette
+      "I'm doing a pirouette"
+    end
+
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
   end
 
-  def jump
-    "Look how high I'm jumping!"
-  end
+  module ClassMethods
 
-  def pirouette
-    "I'm doing a pirouette"
-  end
-
-  def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all."
-  end
- end
-end
-
-module ClassMethods
-
-  def metadata
-    "This class produces objects tha t love to dance."
+    def metadata
+      "This class produces objects that love to dance."
+    end
   end
 end
