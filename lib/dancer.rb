458fe983bc370a258f6dafc827ad21f0bfc18a7f
Hillary Spitzer <hspitzer2@gmail.com>
2019-07-23 15:58:06 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,7 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-class Dance
+class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::ClassMethods
 
